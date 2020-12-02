227d99b3dbd5b4626d4939748962ad15da064ccd
Max Veit <55698339+maxveit@users.noreply.github.com>
2020-12-02 16:30:05 +0100

    Update build_parameters.sh

--- build_parameters.sh
+++ build_parameters.sh
@@ -2,15 +2,11 @@
 
 maven_goals="install"
 
-# Extra args for buildah build and push
-container_build_args=""
-container_push_args=""
-
 image_name="ubi-java-intermediate"
 image_tag="latest"
 image_namespace="cicd"
 
-deployment_create="true"
-deplyoment_namespace="stocktrader"
+#deployment_create="true"
+#deplyoment_namespace="stocktrader"
 
-services_file="services.yaml"
+#services_file="services.yaml"
